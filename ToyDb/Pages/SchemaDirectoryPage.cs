a6abf81e971986248b8a47439bb084aec1f32dfe
joebates25 <josephbates25@gmail.com>
2026-08-17 22:01:00 -0500

    schema direct

--- ToyDb/Pages/SchemaDirectoryPage.cs
+++ ToyDb/Pages/SchemaDirectoryPage.cs
@@ -1,5 +1,4 @@
-﻿using System.Buffers.Binary;
-using System.ComponentModel;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace ToyDb.Pages;
@@ -21,35 +20,76 @@ public class SchemaDirectoryPage(Memory<byte> data) : Page(data), IPageFactory<S
 
         A page number of -1 marks a deleted table.
     */
-    private const int NumTablesOffset = 0;
-
+    private const int SchemaDirectoryPageHeaderSize = sizeof(int);
     private const int DeletedTableValue = -1;
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    private struct SchemaDirectoryPageHeader
+    {
+        internal int NumTables;
+    }
+
+    static SchemaDirectoryPage()
+    {
+        if (SchemaDirectoryPageHeaderSize != Unsafe.SizeOf<SchemaDirectoryPageHeader>())
+            throw new InvalidOperationException("Schema directory page header size is invalid");
+
+        if (!BitConverter.IsLittleEndian)
+            throw new PlatformNotSupportedException("Invalid machine. little endian needed");
+    }
+
+    private ref SchemaDirectoryPageHeader Header =>
+        ref MemoryMarshal.AsRef<SchemaDirectoryPageHeader>(Data.Span);
+
+    private Span<int> SchemaPageNumberSpace =>
+        MemoryMarshal.Cast<byte, int>(Data.Span[SchemaDirectoryPageHeaderSize..]);
 
     public int NumTables
     {
-        get => BinaryPrimitives.ReadInt32LittleEndian(Data.Span[..]);
-        set => BinaryPrimitives.WriteInt32LittleEndian(Data.Span[..], value);
+        get => Header.NumTables;
+        set => Header.NumTables = value;
     }
 
-    public int[] SchemaPageNumbers =>
-        MemoryMarshal.Cast<byte, int>(
-                Data.Span[(NumTablesOffset + sizeof(int))..
-                    (NumTablesOffset + sizeof(int) + NumTables * Constants.PageNumberSize)])
-            .ToArray();
+    public int[] SchemaPageNumbers
+    {
+        get
+        {
+            var numTables = GetValidatedTableCount();
+            return SchemaPageNumberSpace[..numTables].ToArray();
+        }
+    }
 
     public int[] NonDeletedSchemaPageNumbers => SchemaPageNumbers.Where(x => x != DeletedTableValue).ToArray();
 
     public void InsertSchemaDirectoryEntry(int pageNum)
     {
-        BinaryPrimitives.WriteInt32LittleEndian(
-            Data.Span[((NumTablesOffset + 4) + NumTables * Constants.PageNumberSize)..], pageNum);
-        ++NumTables;
+        var numTables = GetValidatedTableCount();
+        if (numTables == SchemaPageNumberSpace.Length)
+            throw new InvalidOperationException("The schema directory page is full.");
+
+        SchemaPageNumberSpace[numTables] = pageNum;
+        NumTables = numTables + 1;
     }
 
     public void ClearSchemaDirectoryEntry(int entrySlot)
     {
-        BinaryPrimitives.WriteInt32LittleEndian(
-            Data.Span[((NumTablesOffset + 4) + entrySlot * Constants.PageNumberSize)..], DeletedTableValue);
+        var numTables = GetValidatedTableCount();
+        if ((uint) entrySlot >= (uint) numTables)
+            throw new ArgumentOutOfRangeException(nameof(entrySlot));
+
+        SchemaPageNumberSpace[entrySlot] = DeletedTableValue;
+    }
+
+    private int GetValidatedTableCount()
+    {
+        var numTables = NumTables;
+        if ((uint) numTables > (uint) SchemaPageNumberSpace.Length)
+        {
+            throw new InvalidDataException(
+                $"Schema directory contains an invalid table count of {numTables}.");
+        }
+
+        return numTables;
     }
 
     public static SchemaDirectoryPage CreatePage(Memory<byte> data)
