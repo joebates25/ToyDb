e2c36ae6dd552ec32e805882e4aca1fbfbbb301a
joebates25 <josephbates25@gmail.com>
2026-08-23 09:59:42 -0500

    small stuff

--- ToyDb/PageBufferManager.cs
+++ ToyDb/PageBufferManager.cs
@@ -15,8 +15,8 @@ public class PageBufferManager : IDisposable
     private readonly Memory<byte> _bufferPool;
 
     private readonly Dictionary<PageNumber, BufferTableEntry> _pageBufferTable = new();
-    private readonly IEvictionPolicy _evictionPolicy;
     private readonly Stack<int> _freeFrames;
+    private readonly IEvictionPolicy _evictionPolicy;
 
     public PageBufferManager(FileIoManager fileIoManager, PageBufferConfig? pageBufferConfig)
     {
@@ -32,7 +32,6 @@ public class PageBufferManager : IDisposable
 
     public async Task<TPage> ReadPageAsync<TPage>(int pageNumber) where TPage : Page, IPageFactory<TPage>
     {
-        // todo: handle full buffer pool
         _logger.Log(LogLevel.Information, $"Reading page {pageNumber}");
         if (_pageBufferTable.TryGetValue(pageNumber, out var frame))
         {
@@ -63,46 +62,14 @@ public class PageBufferManager : IDisposable
         var bufferSlice =
             _bufferPool.Slice(firstFreeFrameNumber * Constants.PageSizeBytes, Constants.PageSizeBytes);
         bufferSlice.Span.Fill(0);
-        _pageBufferTable.Add(pageNumber, BufferTableEntry.Create(firstFreeFrameNumber));
+        _pageBufferTable.Add(pageNumber, BufferTableEntry.CreateDirty(firstFreeFrameNumber));
 
         return TPage.InitializePage(bufferSlice);
     }
 
-    public async Task FlushAsync()
+    public void DirtyPage(int pageNumber)
     {
-        _logger.Log(LogLevel.Information, "Flushing page buffers");
-        foreach (var pageBufferTableEntry in _pageBufferTable)
-        {
-            var frame = pageBufferTableEntry.Value;
-            var pageMemory =
-                (ReadOnlyMemory<byte>) GetBufferFrame(frame.FrameNumber);
-            await _fileIoManager.WriteAsync(pageBufferTableEntry.Key * Constants.PageSizeBytes, pageMemory);
-        }
-
-        await _fileIoManager.FlushAsync();
-    }
-
-    private FrameNumber GetFirstFreeFrameNumber()
-    {
-        if (_freeFrames.Count > 0)
-            return _freeFrames.Pop();
-
-        if (TryEvictPage(out var freeFrameNumber))
-            return freeFrameNumber;
-
-        throw new NotImplementedException(
-            "todo: Need to handle case when all frames are in use and no more buffer space available");
-    }
-
-    private bool HasPage(int pageNumber) => _pageBufferTable.ContainsKey(pageNumber);
-
-    private Memory<byte> GetBufferFrame(int frameNumber) =>
-        _bufferPool.Slice(frameNumber * Constants.PageSizeBytes, Constants.PageSizeBytes);
-
-    public void Dispose()
-    {
-        _fileIoManager.FlushAsync().GetAwaiter().GetResult();
-        _fileIoManager.Dispose();
+        // Do NOT call MarkPageNotInUse because dirty pages shouldn't be evicted. Write them to disk instead.
     }
 
     // todo: page probably needs page number at this point
@@ -120,6 +87,38 @@ public class PageBufferManager : IDisposable
         }
     }
 
+    public async Task FlushAsync()
+    {
+        _logger.Log(LogLevel.Information, "Flushing page buffers");
+        foreach (var pageBufferTableEntry in _pageBufferTable)
+        {
+            var frame = pageBufferTableEntry.Value;
+            var pageMemory =
+                (ReadOnlyMemory<byte>) GetBufferFrame(frame.FrameNumber);
+            await _fileIoManager.WriteAsync(pageBufferTableEntry.Key * Constants.PageSizeBytes, pageMemory);
+        }
+
+        await _fileIoManager.FlushAsync();
+    }
+
+    public void Dispose()
+    {
+        _fileIoManager.FlushAsync().GetAwaiter().GetResult();
+        _fileIoManager.Dispose();
+    }
+
+    private FrameNumber GetFirstFreeFrameNumber()
+    {
+        if (_freeFrames.Count > 0)
+            return _freeFrames.Pop();
+
+        if (TryEvictPage(out var freeFrameNumber))
+            return freeFrameNumber;
+
+        throw new NotImplementedException(
+            "todo: Need to handle case when all frames are in use and no more buffer space available");
+    }
+
     private bool TryEvictPage(out int evictFrame)
     {
         if (!_evictionPolicy.TryEvict(out evictFrame)) return false;
@@ -131,11 +130,18 @@ public class PageBufferManager : IDisposable
         _pageBufferTable.Remove(pageNumber);
         return true;
     }
+
+    private bool HasPage(int pageNumber) => _pageBufferTable.ContainsKey(pageNumber);
+
+    private Memory<byte> GetBufferFrame(int frameNumber) =>
+        _bufferPool.Slice(frameNumber * Constants.PageSizeBytes, Constants.PageSizeBytes);
 }
 
 public record BufferTableEntry(int FrameNumber, bool Dirty, int PinCount)
 {
     public static BufferTableEntry Create(int frameNumber) => new(frameNumber, false, 1);
+
+    public static BufferTableEntry CreateDirty(int frameNumber) => new(frameNumber, true, 1);
 }
 
 public record PageBufferConfig(int FrameCount);
